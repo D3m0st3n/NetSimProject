635085ea50d8d820616f7c77f44065ecde2addbc
Alexandre Bouillon <Alex@tsf-460-wpa-4-220.epfl.ch>
2018-10-23 15:51:04 +0200

    first upload of base file sfor the project

---
+++ src/network.cpp
@@ -0,0 +1,43 @@
+#include "network.h"
+
+void Network::resize (const size_t&) {}
+
+bool Network::add_link (const size_t&, const size_t&) {}
+
+size_t Network::random_connect(const double&) {}
+
+size_t Network::set_values(const std::vector<double>&) {}
+
+size_t Network::size() const { return values.size();}
+
+size_t Network::degree(const size_t &_n) const {}
+
+double Network::value(const size_t &_n) const {
+	if (not(values.empty())) {
+		for (size_t i(0); i<values.size(); ++i) {
+			if (i==_n) { return values[i];}
+			}
+		}
+	/*How to manage empty vectors???*/
+	}
+
+std::vector<double> Network::sorted_values() const {
+	std::vector<double> _sorted(values);
+	if (not(values.empty())) {
+		
+		double k;
+		
+		for (size_t i(0); i<_sorted.size(); ++i) {
+			for (size_t j(i); j<_sorted.size(); ++j) {
+				if (i<j) {k=_sorted[i];
+					_sorted[i]=_sorted[j];
+					_sorted[j]=k;}
+				}
+			}
+		return _sorted;
+		}
+		
+	else return _sorted;
+	}
+
+std::vector<size_t> Network::neighbors(const size_t&) const {}
