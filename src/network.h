0f1b406524d11dbc27ec513e98175e014a728303
Alexandre Bouillon <Alex@MacBook-Pro-de-Alexandre.local>
2018-10-27 19:10:45 +0200

    comment update

--- src/network.h
+++ src/network.h
@@ -12,16 +12,19 @@ class Network {
 public:
     Network() {}
 
-/*! Resizes the list of nodes (\ref values) */
+/*! 
+     Resizes the list of nodes (\ref values) and also resets all values.
+     After this function is called \ref values has size *n* and contains random numbers (normal distribution, mean=0, sd=1).
+*/
     void resize(const size_t&); /*Exception to manage*/
 /*! Adds a bidirectional link between two nodes
   @param[in] a,b the indexes if the two nodes 
-  @param[out] success the link was succesfully inserted
+  @param[out] success the link was succesfully inserted (true if both nodes exist and the link did not exist yet)
  */
-    bool add_link(const size_t&, const size_t&); /*DONE*/
-/*! Creates random connections between nodes: each node *n* will be linked with *degree(n)* other nodes (randomly chosen) where *degree(n)* is Poisson-distributed.
-
-  All previous links are cleared first.
+    bool add_link(const size_t&, const size_t&); /**/
+/*! Creates random connections between nodes: each node *n* will be linked with *degree(n)* other nodes (randomly chosen),
+ where *degree(n)* is Poisson-distributed.
+  All previous links are erased first.
   @param mean_deg the average of the Poisson distribution.
  */
     size_t random_connect(const double&);
